53268711b1b5d08a7d6cc4c6a72f097f9e9fc940
Roman <CLA0039@nhs.vic.edu.au>
2025-10-08 14:40:13 +1100

    'hii'

--- scripts/giving_way_scene_1.gd
+++ scripts/giving_way_scene_1.gd
@@ -21,16 +21,13 @@ func _process(delta):
 			%radio.visible = true
 			radioOpen = true
 
-	
 func _on_area_3d_area_entered(area):
 	if area.has_method("isCar"):
 		isDead = true
 
-
 func _on_area_3d_2_area_entered(area):
 	if area.has_method("isCar"):
 		isDead = true
-
 
 func _on_area_3d_3_area_entered(area):
 	if area.has_method("isCar"):
@@ -40,18 +37,15 @@ func _on_area_3d_4_area_entered(area):
 	if area.has_method("isCar"):
 		isDead = true
 
-
 func _on_button_2_pressed():
 	isDead = false
 	%gameOver.visible = false
 	get_tree().paused = false
 	get_tree().reload_current_scene()
 
-
 func _on_enemy_car_area_entered(area):
 	if area.has_method("isCar"):
 		isDead = true
-
 
 func _on_quit_button_pressed():
 	get_tree().paused = false
