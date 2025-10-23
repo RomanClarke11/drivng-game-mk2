2788c5514e356e355421c13eb48433d23294587d
Roman <CLA0039@nhs.vic.edu.au>
2025-10-23 11:38:15 +1100

    roman

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -1,6 +1,8 @@
 extends Control
-
-
+var hiden = false
+func _ready():
+	%buttons.visible = true
+	%"roman label".visible = false
 func _on_play_pressed():
 	get_tree().change_scene_to_file("res://scenes/level_menu.tscn")
 
@@ -13,3 +15,14 @@ func _on_settings_pressed():
 
 func _on_quit_pressed():
 	get_tree().quit()
+
+
+func _on_button_pressed():
+	if hiden == false:
+		%buttons.visible = false
+		%"roman label".visible = true
+		hiden = true
+	elif hiden == true:
+		%buttons.visible = true
+		%"roman label".visible = false
+		hiden = false
